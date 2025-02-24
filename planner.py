ce2ab27eee2ce8487da1a0ee76837a0995efcd6c
Jacob Leary <jackal@iastate.edu>
2025-02-24 13:59:31 -0600

    smaller chage, needs to be read through and checked

--- planner.py
+++ planner.py
@@ -9,13 +9,27 @@ def AStar(grid, start, end):
     visited = set()
     parent = {start: None}
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-    while true:
-        min = (0,0)
-        weight = infinity
-        for node in frontier:
-            for dir in directions:
-                if dir not in frontier and dir not in explored:
-                    
+    weight = 1
+    while frontier:
+        frontier.sort(key=lambda x: weight * (abs(x[0] - end[0]) + abs(x[1] - end[1])))
+        x, y = frontier.pop(0)
+        if (x, y) == end:
+            path = []
+            while (x, y) is not None:
+                path.append((x, y))
+                if parent[(x, y)] is None:
+                    break
+                x, y = parent[(x, y)]
+            return path[::-1]
+        if (x, y) in visited:
+            continue
+        visited.add((x, y))
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == 0 and (nx, ny) not in visited:
+                frontier.append((nx, ny))
+                parent[(nx, ny)] = (x, y)
+
 
         
 
