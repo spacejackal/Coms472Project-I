19d0bfe6a7087887259d01f8b2b15d32e418af20
Jacob Leary <jackal@iastate.edu>
2025-02-28 13:51:37 -0600

    after testing more logic incorperating old weight values is needed

--- devel.py
+++ devel.py
@@ -4,8 +4,14 @@ import scipy
 from main import Task
 
 # example of testing a specific task
-id = 6
+id = 98
 T = Task(id)                # initialize task
 T.plan_path()               # path planning
 T.visualize_path()          # path visualization
 print (T.check_path())      # the validity check of the generated path
+
+# for id in range(0,99):
+#     T = Task(id)                # initialize task
+#     T.plan_path()               # path planning
+#     T.visualize_path()          # path visualization
+#     print (T.check_path())      # the validity check of the generated path
